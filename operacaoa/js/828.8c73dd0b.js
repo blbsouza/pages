ff7768583df4c56e225cd068c5d413a6e31c15bf
blbtech <88602824+blbsouza@users.noreply.github.com>
2025-02-22 21:18:22 -0300

    add operacaoa finished

---
+++ operacaoa/js/828.8c73dd0b.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkoperacaoa"]=self["webpackChunkoperacaoa"]||[]).push([[828],{4828:function(e){e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=828.8c73dd0b.js.map
